643c0e99124c4598a2d3802f65365a87bd6dedca
Lucas de Brito <Lucas@Lucas-de-Brito.local>
2018-04-14 01:34:48 -0300

    View preview do deputado construida e outlets e actions setados

--- deputadosAP12/DeputadoPreViewController.swift
+++ deputadosAP12/DeputadoPreViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class DeputadoPreViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    //RECEBER ID DO DEPUTADO PARA FAZER CALCULO DA COR DE FUNDO
+    
+    // MARK: - Outlets
+    @IBOutlet weak var fotoDeputado: UIImageView!
+    @IBOutlet weak var nomeDeputado: UILabel!
+    @IBOutlet weak var partidoEstadoDeputado: UILabel!
+    @IBOutlet weak var situacaoDeputado: UILabel!
+    @IBOutlet weak var inicioMandatoDeputado: UILabel!
+    @IBOutlet weak var seguirDeputadoOutlet: UIButton!
+    
+    
+    // MARK: - Actions
+    @IBAction func seguirDeputadoAction(_ sender: Any) {
+    }
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +36,9 @@ class DeputadoPreViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+}
+
+extension DeputadoPreViewController{
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
