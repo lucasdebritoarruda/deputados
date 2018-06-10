51f831fdb5fbad425cc17f0c80f3cf1a8bb2766a
Lucas de Brito <Lucas@Lucas-de-Brito.local>
2018-06-10 01:57:05 -0300

    Adiciona View de detalhamento de informaçoes

--- deputadosAP12/infoTableViewController.swift
+++ deputadosAP12/infoTableViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+
 class infoTableViewController: UITableViewController {
     
+    // MARK: - Properties
+    
     var itens = ["O QUE FAZ UM DEPUTADO FEDERAL?","QUAIS SÃO AS ATRIBUIÇÕES DO CARGO?","COMO ELES SÃO ELEITOS?", "QUEM PODE SER DEPUTADO FEDERAL?","QUANTO GANHA UM DEPUTADO FEDERAL?"]
+    var index:Int?
+    
+    
+    // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,67 +29,42 @@ class infoTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
 
+}
 
+// MARK: - Navigation
+
+extension infoTableViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        index = indexPath.row
+        self.performSegue(withIdentifier: "infoSegue", sender: self)
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "infoSegue"{
+            let controller = segue.destination as! infoDetailViewController
+            controller.index = index
+            let infoButton = UIBarButtonItem()
+            infoButton.title = "Informações"
+            navigationItem.backBarButtonItem = infoButton
+        }
+    }
+}
+
+// MARK: - Table View Data Source
+
+extension infoTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return itens.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "infoCell")
         
         cell.textLabel?.text = itens[indexPath.row].lowercased().capitalizingFirstLetter()
-
+        
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
